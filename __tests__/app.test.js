827806b347cb7ebb8fc6a672da87dc8c65027ff3
satyadvaya <satyadvaya@gmail.com>
2021-09-21 17:12:59 -0700

    passing index.html return from GET / test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,14 @@
-import request from 'supertest';
-import { readFile } from 'fs/promises';
-import app from './lib/app.js';
+const request = require('supertest');
+const { readFile } = require('fs/promises');
+const app = require('../lib/app.js');
 
 describe('HTTP routes', () => {
-  it('should return index.html from GET /', async () => {
-    const [response, index] = await Promise.all([
-      request(app).get('/'),
-      readFile('./public/index.html', 'utf-8'),
-    ]);
+    it('should return index.html from GET /', async () => {
+        const [response, index] = await Promise.all([
+            request(app).get('/'),
+            readFile('./public/index.html', 'utf-8'),
+        ]);
 
-    expect(response.text).toEqual(index);
-  });
+        expect(response.text).toEqual(index);
+    });
 });
